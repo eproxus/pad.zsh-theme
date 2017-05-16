0325aebe2ed0b132d2000b3e2f98239f22a0ee15
Adam Lindberg <hello@alind.io>
2017-05-16 18:02:04 +0200

    Remove install script

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -e
-
-echo "Creating symlink in .oh-my-zsh/themes..."
-ln pad.zsh-theme ~/.oh-my-zsh/themes/pad.zsh-theme
-echo "Successfully created ~/.oh-my-zsh/themes/pad.zsh-theme!"
